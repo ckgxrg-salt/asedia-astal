3ab84c91e347af0b046f616ce55045b75cecd36d
ckgxrg <ckgxrg@ckgxrg.io>
2025-06-15 20:13:14 +0800

    common: add states and mpris

--- app.ts
+++ app.ts
@@ -1,10 +1,7 @@
-import app from "ags/gtk4/app"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+import app from "ags/gtk4/app";
+import style from "./style.scss";
 
 app.start({
   css: style,
-  main() {
-    app.get_monitors().map(Bar)
-  },
-})
+  main() {},
+});
---
+++ common/states.ts
@@ -0,0 +1,10 @@
+import { createState } from "ags";
+
+// Whether to inhibit screen locker
+export const [inhibit, setInhibit] = createState(false);
+
+// Which one to show in the bottom row
+// - 0 for Media
+// - 1 for Weather
+// - 2 for Tray
+export const [bottom, setBottom] = createState(2);
